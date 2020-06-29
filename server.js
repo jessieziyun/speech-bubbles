4b4ce60b53d94daa9fd9926020a5bdf6624077f3
Jessie Zhang <jessiezhang98@gmail.com>
2020-06-29 16:56:47 +0100

    deleted obsolete socket.io code

--- server.js
+++ server.js
@@ -7,17 +7,11 @@ const app = express();
 const port = process.env.PORT || 4027;
 const server = http.createServer(app);
 
-const io = socket.listen(server);
-
 app.use(express.static("public"));
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
     response.sendFile(__dirname + "/views/index.html");
-});
-
-io.on("connection", socket => {
-    console.log(`New connection: ${socket.id}`);
 });
 
 // listen for requests :)
