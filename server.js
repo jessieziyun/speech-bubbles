5b141cd3672adef1404c5a61533b1828c3cb392e
Jessie Zhang <jessiezhang98@gmail.com>
2020-07-15 22:13:54 +0100

    added Procfile and adjusted routing

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const http = require("http");
 
 const app = express();
 const port = process.env.PORT || 3000;
+const base = process.env.BASE || "/";
 const server = http.createServer(app);
 
-app.use(process.env.BASE, express.static("public"));
+app.use(base, express.static("public"));
 
 // https://expressjs.com/en/starter/basic-routing.html
-app.get(process.env.BASE, (request, response) => {
+app.get(base, (request, response) => {
     response.sendFile(__dirname + "/views/index.html");
 });
 
